343baae2b38f2a4d1429a37eeabc0c0a5dd04c13
scfmod <45406107+scfmod@users.noreply.github.com>
2025-09-11 00:30:29 +0200

    Added FillUnitExtension specialization

---
+++ docs/FILLUNIT_EXTENSION.md
@@ -0,0 +1,88 @@
+# FillUnitExtension
+
+## Add specialization to vehicle type
+
+```xml
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<modDesc version="...">
+    ...
+    <vehicleTypes>
+        <!-- Extend parent type, can be anything -->
+        <type name="..." parent="..." className="..." filename="...">
+            ...
+            <!-- Add entry after fillUnit specialization -->
+            <specialization name="FS25_0_MaterialProcessor.fillUnitExtension" />
+        </type>
+    </vehicleTypes>
+    ...
+</modDesc>
+```
+
+## FillUnit
+
+```
+vehicle.fillUnitExtension.fillUnit(%)
+```
+
+### Attributes
+| Name      | Type  | Required | Default | Description              |
+|-----------|-------|----|-----------|------------------------------|
+| fillUnitIndex | integer | Yes | | |
+
+
+## Fill level object changes
+
+```
+vehicle.fillUnitExtension.fillUnit(%).fillLevelObjectChanges
+```
+
+Trigger object changes based on the fill level percentage.
+
+### Attributes
+| Name      | Type  | Required | Default | Description              |
+|-----------|-------|----|-----------|------------------------------|
+| threshold | float | No | ```0.9```| Defines at which fillUnit fill level percentage the object changes |
+
+
+### Example
+```xml
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<vehicle>
+    <fillUnitExtension>
+        <fillUnit fillUnitIndex="3">
+            <fillLevelObjectChanges threshold="0.85">
+                <objectChange node="alarmBeacon" visibilityActive="true" visibilityInactive="false" />
+            </fillLevelObjectChanges>
+        </fillUnit>
+    </fillUnitExtension>
+</vehicle>
+```
+
+## Fill level sound
+
+```
+vehicle.fillUnitExtension.fillUnit(%).fillLevelSound
+```
+
+Play sound depending on the fill level percentage.
+Same as a normal vehicle sample entry, but with additional attributes.
+
+### Attributes
+| Name      | Type  | Required | Default | Description              |
+|-----------|-------|----|-----------|------------------------------|
+| threshold | float | No | ```0.9```| Defines at which fillUnit fill level percentage the sound is triggered |
+| thresholdIsGreater | boolean | No  | ```true``` | If true the sound is triggered above threshold, if not then below threshold |
+| requiresPoweredOn | boolean | No | ```true``` | |
+| requiresTurnedOn | boolean | No | ```false``` | |
+
+### Example
+```xml
+<?xml version="1.0" encoding="utf-8" standalone="no"?>
+<vehicle>
+    <fillUnitExtension>
+        <fillUnit fillUnitIndex="3">
+            <fillLevelSound threshold="0.95" template="rollbeltAlarm" linkNode="alarmSoundNode" />
+        </fillUnit>
+    </fillUnitExtension>
+</vehicle>
+```
---
+++ scripts/specializations/FillUnitExtension.lua
@@ -0,0 +1,141 @@
+---@class FillUnitEntry
+---@field fillUnitIndex number
+---@field soundThreshold number
+---@field soundThresholdIsGreater boolean
+---@field soundRequiresTurnedOn boolean
+---@field soundRequiresPoweredOn boolean
+---@field sample? table
+---@field fillLevelObjectChangeThreshold number
+---@field fillLevelObjectChanges? table
+
+---@class FillUnitExtension_spec
+---@field fillUnitEntries FillUnitEntry[]
+
+---@class FillUnitExtension : VehicleObject
+FillUnitExtension = {}
+
+---@type string
+FillUnitExtension.SPEC_NAME = 'spec_' .. g_currentModName .. '.fillUnitExtension'
+FillUnitExtension.MOD_NAME = g_currentModName
+
+function FillUnitExtension.prerequisitesPresent(specializations)
+    return SpecializationUtil.hasSpecialization(FillUnit, specializations)
+end
+
+function FillUnitExtension.initSpecialization()
+    ---@type XMLSchema
+    local schema = Vehicle.xmlSchema
+    local basePath = 'vehicle.fillUnitExtension.fillUnit(?)'
+    schema:setXMLSpecializationType('FillUnitExtension')
+
+    schema:register(XMLValueType.INT, basePath .. '#fillUnitIndex')
+
+    SoundManager.registerSampleXMLPaths(schema, basePath, "fillLevelSound")
+    schema:register(XMLValueType.BOOL, basePath .. '.fillLevelSound#requiresTurnedOn', '', false)
+    schema:register(XMLValueType.BOOL, basePath .. '.fillLevelSound#requiresPoweredOn', '', true)
+    schema:register(XMLValueType.FLOAT, basePath .. '.fillLevelSound#threshold', '', 0.9)
+    schema:register(XMLValueType.BOOL, basePath .. '.fillLevelSound#thresholdIsGreater', '', true)
+
+    schema:register(XMLValueType.FLOAT, basePath .. '.fillLevelObjectChanges#threshold', 'Defines at which fillUnit fill level percentage the object changes', 0.9)
+    ObjectChangeUtil.registerObjectChangeXMLPaths(schema, basePath .. ".fillLevelObjectChanges")
+
+    schema:setXMLSpecializationType()
+end
+
+function FillUnitExtension.registerEventListeners(vehicleType)
+    SpecializationUtil.registerEventListener(vehicleType, 'onLoad', FillUnitExtension)
+    SpecializationUtil.registerEventListener(vehicleType, 'onDelete', FillUnitExtension)
+    SpecializationUtil.registerEventListener(vehicleType, 'onUpdateTick', FillUnitExtension)
+end
+
+function FillUnitExtension:onLoad()
+    ---@type FillUnitExtension_spec
+    local spec = self[FillUnitExtension.SPEC_NAME]
+
+    spec.fillUnitEntries = {}
+
+    ---@type XMLFile
+    local xmlFile = self.xmlFile
+
+    xmlFile:iterate('vehicle.fillUnitExtension.fillUnit', function (_, key)
+        local entry = {}
+        ---@cast entry FillUnitEntry
+
+        entry.fillUnitIndex = xmlFile:getValue(key .. '#fillUnitIndex')
+
+        if not self:getFillUnitExists(entry.fillUnitIndex) then
+            Logging.xmlError(xmlFile, 'Invalid fillUnitIndex (%s)', key .. '#fillUnitIndex')
+            return
+        end
+
+        entry.soundRequiresPoweredOn = xmlFile:getValue(key .. '.fillLevelSound#requiresPoweredOn', true)
+        entry.soundRequiresTurnedOn = xmlFile:getValue(key .. '.fillLevelSound#requiresTurnedOn', false)
+        entry.soundThreshold = xmlFile:getValue(key .. '.fillLevelSound#threshold', 0.9)
+        entry.soundThresholdIsGreater = xmlFile:getValue(key .. '.fillLevelSound#thresholdIsGreater', true)
+        entry.sample = g_soundManager:loadSampleFromXML(xmlFile, key, "fillLevelSound", self.baseDirectory, self.components, 0, AudioGroup.VEHICLE, self.i3dMappings, self)
+
+        entry.fillLevelObjectChanges = {}
+        ObjectChangeUtil.loadObjectChangeFromXML(xmlFile, key .. '.fillLevelObjectChanges', entry.fillLevelObjectChanges, self.components, self)
+
+        if #entry.fillLevelObjectChanges == 0 then
+            entry.fillLevelObjectChanges = nil
+        else
+            entry.fillLevelObjectChangeThreshold = xmlFile:getValue(key .. '.fillLevelObjectChanges#threshold', 0.9)
+            ObjectChangeUtil.setObjectChanges(entry.fillLevelObjectChanges, false, self, self.setMovingToolDirty)
+        end
+
+        table.insert(spec.fillUnitEntries, entry)
+    end)
+end
+
+function FillUnitExtension:onDelete()
+    ---@type FillUnitExtension_spec
+    local spec = self[FillUnitExtension.SPEC_NAME]
+
+    for _, entry in ipairs(spec.fillUnitEntries) do
+        g_soundManager:deleteSample(entry.sample)
+    end
+end
+
+function FillUnitExtension:onUpdateTick(dt)
+    ---@type FillUnitExtension_spec
+    local spec = self[FillUnitExtension.SPEC_NAME]
+
+    for _, entry in ipairs(spec.fillUnitEntries) do
+        if entry.fillLevelObjectChanges ~= nil then
+            local fillLevelPct = self:getFillUnitFillLevelPercentage(entry.fillUnitIndex) or 0
+            ObjectChangeUtil.setObjectChanges(entry.fillLevelObjectChanges, fillLevelPct > entry.fillLevelObjectChangeThreshold, self, self.setMovingToolDirty)
+        end
+
+        FillUnitExtension.updateFillLevelSound(self, entry)
+
+        if self.isClient then
+            if entry.sample ~= nil and g_soundManager:getIsSamplePlaying(entry.sample) then
+                self:raiseActive()
+            end
+        end
+    end
+end
+
+---@param entry FillUnitEntry
+function FillUnitExtension:updateFillLevelSound(entry)
+    if not self.isClient or not entry.sample then
+        return
+    end
+
+    local fillLevelPct = self:getFillUnitFillLevelPercentage(entry.fillUnitIndex) or 0
+    local playSample = (entry.soundThresholdIsGreater and fillLevelPct > entry.soundThreshold) or (not entry.soundThresholdIsGreater and fillLevelPct < entry.soundThreshold)
+    local isPlaying = g_soundManager:getIsSamplePlaying(entry.sample)
+
+    if entry.soundRequiresTurnedOn and self.getIsTurnedOn ~= nil and not self:getIsTurnedOn() then
+        playSample = false
+    elseif entry.soundRequiresPoweredOn and not self:getIsPowered() then
+        playSample = false
+    end
+
+    if playSample and not isPlaying then
+        g_soundManager:playSample(entry.sample)
+    elseif not playSample and isPlaying then
+        g_soundManager:stopSample(entry.sample)
+    end
+end
